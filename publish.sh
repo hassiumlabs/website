ca142953da70fc713dbf13638a15ff6441a8f01e
Andy Spohn <andy@hassiumlabs.com>
2016-03-20 19:27:51 -0400

    Google Analytics and contact page info

--- publish.sh
+++ publish.sh
@@ -4,7 +4,7 @@
 #
 
 # Sync with temp area on hassiumlabs.com server
-rsync -avzd -e ssh content hldc:/tmp
+rsync -avz --delete -e ssh content hldc:/tmp
 
 # Sync from the temp area into the site web root
-ssh hldc -C 'sudo rsync -avzd /tmp/content/ /var/www/hassiumlabs.com'
+ssh hldc -C 'sudo rsync -avz --delete /tmp/content/ /var/www/hassiumlabs.com'
