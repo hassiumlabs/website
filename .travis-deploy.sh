280372f688b7a3a1162fdf3d393a7eabb172106f
Andy Spohn <andy@hasssiumlabs.com>
2016-10-08 12:08:53 -0400

    refacter to use hugo to generate site content

--- .travis-deploy.sh
+++ .travis-deploy.sh
@@ -8,9 +8,9 @@
 pip install --user awscli; export PATH=$PATH:$HOME/.local/bin
 
 if [ "$TRAVIS_BRANCH" == "master" ]; then
-  aws s3 sync content/ s3://hassiumlabs-website/production/site/ --delete
+  aws s3 sync public/ s3://hassiumlabs-website/production/site/ --delete
 fi
 
 if [ "$TRAVIS_BRANCH" == "test" ]; then
-  aws s3 sync content/ s3://hassiumlabs-website/test/site/ --delete
+  aws s3 sync public/ s3://hassiumlabs-website/test/site/ --delete
 fi
